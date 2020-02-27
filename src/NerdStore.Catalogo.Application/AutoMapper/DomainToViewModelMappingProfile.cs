73e6b0e34cb360abc4f07915a089389158d44d63
Wilker Alves Nogueira <wilkeralves97@gmail.com>
2020-02-27 14:45:31 -0300

    Criando a camada de application

---
+++ src/NerdStore.Catalogo.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            //Onde eu tenho largura no ProdutoViewModel eu vou mapear de Produto.Dimenssoes.Largura assim para os demais campos
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(d => d.Largura, o => o.MapFrom(s => s.Dimensoes.Largura))
+                .ForMember(d => d.Altura, o => o.MapFrom(s => s.Dimensoes.Altura))
+                .ForMember(d => d.Profundidade, o => o.MapFrom(s => s.Dimensoes.Profundidade));
+
+            //Todos os campos de categoria serão mapeados para categoria view model
+            CreateMap<Categoria, CategoriaViewModel>();
+        }
+    }
+}
---
+++ src/NerdStore.Catalogo.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Application.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<ProdutoViewModel, Produto>()
+                .ConstructUsing(p =>
+                    new Produto(p.Nome, p.Descricao, p.Ativo,
+                        p.Valor, p.CategoriaId, p.DataCadastro,
+                        p.Imagem, new Dimensoes(p.Altura, p.Largura, p.Profundidade)));
+
+
+            //A classe categorio só pode ser criada via construtor
+            //Nesse caso estou mapeando CategoriaViewModel passando os dados para Categoria via construtor
+            CreateMap<CategoriaViewModel, Categoria>()
+                .ConstructUsing(c => new Categoria(c.Nome, c.Codigo));
+
+            
+
+
+        }
+    }
+}
