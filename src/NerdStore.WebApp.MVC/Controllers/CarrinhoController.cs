d41ce6c8b1b11285c36da6deae92c23032dc21cb
Wilker Alves <wilkeralves97@outlook.com>
2020-03-07 10:57:32 -0300

    imcompleto

--- src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
+++ src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -8,25 +8,31 @@ using NerdStore.Catalogo.Application.Services;
 using NerdStore.Core.Communication.Mediator;
 using NerdStore.Core.Messages.CommonMessages.Notifications;
 using NerdStore.Vendas.Application.Commands;
+using NerdStore.Vendas.Application.Queries;
 
 namespace NerdStore.WebApp.MVC.Controllers
 {
     public class CarrinhoController : ControllerBase
     {
         private readonly IProdutoAppService _produtoAppService;
+        private readonly IPedidoQueries _pedidoQueries;
         private readonly IMediatorHandler _mediatorHandler;
 
         public CarrinhoController(INotificationHandler<DomainNotification> notifications,
                                   IProdutoAppService produtoAppService,
-                                  IMediatorHandler mediatorHandler) : base(mediatorHandler, notifications)
+                                  IMediatorHandler mediatorHandler, 
+                                  IPedidoQueries pedidoQueries) : base(mediatorHandler, notifications)
         {
             _produtoAppService = produtoAppService;
             _mediatorHandler = mediatorHandler;
+            _pedidoQueries = pedidoQueries;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        [Route("meu-carrinho")]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
         }
 
         [HttpPost]
@@ -54,5 +60,47 @@ namespace NerdStore.WebApp.MVC.Controllers
             TempData["Erros"] = ObterMensagensErro();
             return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
         }
+
+        //[HttpPost]
+        //[Route("remover-item")]
+        //public async Task<IActionResult> RemoverItem(Guid id)
+        //{
+        //    var produto = await _produtoAppService.ObterPorId(id);
+        //    if (produto is null) return BadRequest();
+
+        //    var command = new RemoverItemPedidoCommand(ClienteId, id);
+        //    await _mediatorHandler.EnviarComando(command);
+
+        //    if (OperacaoValida()) return RedirectToAction("Index");
+
+        //    return View("Index", await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
+        //}
+
+        //[HttpPost]
+        //[Route("atualizar-item")]
+        //public async Task<IActionResult> AtualizarItem(Guid id, int quantidade)
+        //{
+        //    var produto = await _produtoAppService.ObterPorId(id);
+        //    if (produto is null) return BadRequest();
+
+        //    var command = new AtualizarItemPedidoCommand(ClienteId, id, quantidade);
+        //    await _mediatorHandler.EnviarComando(command);
+
+        //    if (OperacaoValida()) return RedirectToAction("Index");
+
+        //    return View("Index", await _pedidoQueries.ObterCarrinhoCliente(ClienteId))
+        //}
+
+        //[HttpPost]
+        //[Route("aplicar-voucher")]
+        //public async Task<IActionResult> AplicarVoucher(string voucherCodigo)
+        //{
+        //    var command = new AplicarVoucherPedidoCommand(ClienteId, voucherCodigo);
+        //    await _mediatorHandler.EnviarComando(command);
+
+        //    if (OperacaoValida()) return RedirectToAction("Index");
+
+        //    return View("Index", await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
+        //}
     }
 }
